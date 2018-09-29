671f0faad74e7d72752df910b2ad305a5b0eeda2
Jerason Banes <jbanes@gmail.com>
2018-09-29 00:10:31 -0500

    Added CPU Type to the menu.

--- dosbox/src/gui/sdl_menu.cpp
+++ dosbox/src/gui/sdl_menu.cpp
@@ -29,6 +29,9 @@
 #include "render.h"
 #include "cpu.h"
 
+#define MENU_ITEMS 6 
+
+extern Bitu CPU_extflags_toggle;
 
 bool menu_active = false;
 bool menu_last = false;
@@ -41,15 +44,17 @@ struct MENU_Block
     char *frameskip;
     char *cycles;
     char *core;
+    char *cpuType;
 };
 
 static MENU_Block menu;
 
-const char *menuoptions[5] = {
+const char *menuoptions[MENU_ITEMS] = {
     "Resume",
     "Frameskip: ",
     "Cycles: ",
     "CPU Core: ",
+    "CPU Type: ",
     "Exit"
 };
 
@@ -65,6 +70,7 @@ void MENU_Init(int bpp)
     menu.frameskip = (char*)malloc(16);
     menu.cycles = (char*)malloc(16);
     menu.core = (char*)malloc(16);
+    menu.cpuType = (char*)malloc(16);
 }
 
 void MENU_Deinit()
@@ -74,12 +80,15 @@ void MENU_Deinit()
 
 void MENU_UpdateMenu()
 {
+    // Frameskip
     sprintf(menu.frameskip, "%i", render.frameskip.max);
     
+    // CPU Cycles
     if(CPU_AutoDetermineMode & CPU_AUTODETERMINE_CYCLES) strcpy(menu.cycles, "auto");
     else if(CPU_CycleAutoAdjust) strcpy(menu.cycles, "max");
     else sprintf(menu.cycles, "%i", CPU_CycleMax);
     
+    // CPU Core
     if(CPU_AutoDetermineMode & CPU_AUTODETERMINE_CORE) strcpy(menu.core, "auto");
     else if(cpudecoder == &CPU_Core_Normal_Run) strcpy(menu.core, "normal");
     else if(cpudecoder == &CPU_Core_Simple_Run) strcpy(menu.core, "simple");
@@ -87,9 +96,20 @@ void MENU_UpdateMenu()
 #if (C_DYNREC)
     else if(cpudecoder == &CPU_Core_Dynrec_Run) strcpy(menu.core, "dynamic");
 #endif
+    else if(cpudecoder ==  &CPU_Core_Prefetch_Run) strcpy(menu.core, "prefetch");
     else strcpy(menu.core, "unknown");
     
     if(CPU_AutoDetermineMode & CPU_AUTODETERMINE_CORE) strcpy(menu.core, "auto");
+    
+    // CPU Type
+    if(CPU_ArchitectureType == CPU_ARCHTYPE_MIXED) strcpy(menu.cpuType, "auto");
+    else if(CPU_ArchitectureType == CPU_ARCHTYPE_386FAST && cpudecoder == &CPU_Core_Prefetch_Run) strcpy(menu.cpuType, "386 (Prefetch)");
+    else if(CPU_ArchitectureType == CPU_ARCHTYPE_386FAST) strcpy(menu.cpuType, "386");
+    else if(CPU_ArchitectureType == CPU_ARCHTYPE_386SLOW) strcpy(menu.cpuType, "386 (Slow)");
+    else if(CPU_ArchitectureType == CPU_ARCHTYPE_486NEWSLOW && cpudecoder == &CPU_Core_Prefetch_Run) strcpy(menu.cpuType, "486 (Prefetch)");
+    else if(CPU_ArchitectureType == CPU_ARCHTYPE_486NEWSLOW) strcpy(menu.cpuType, "486 (Slow)");
+    else if(CPU_ArchitectureType == CPU_ARCHTYPE_PENTIUMSLOW) strcpy(menu.cpuType, "Pentium (Slow)");
+    else strcpy(menu.cpuType, "Unknown");
 }
 
 void MENU_Toggle()
@@ -107,8 +127,8 @@ void MENU_MoveCursor(int direction)
 {
     menu.selected += direction;
     
-    if(menu.selected < 0) menu.selected = 4;
-    if(menu.selected > 4) menu.selected = 0;
+    if(menu.selected < 0) menu.selected = MENU_ITEMS-1;
+    if(menu.selected > MENU_ITEMS-1) menu.selected = 0;
 }
 
 void MENU_Activate()
@@ -174,7 +194,48 @@ void MENU_Activate()
             
             break;
             
-        case 4: // Exit
+        case 4: // CPU Type
+            
+            if(CPU_ArchitectureType == CPU_ARCHTYPE_MIXED)
+            {
+                CPU_ArchitectureType = CPU_ARCHTYPE_386FAST;
+                CPU_PrefetchQueueSize = 16;
+                cpudecoder = &CPU_Core_Prefetch_Run;
+            }
+            else if(CPU_ArchitectureType == CPU_ARCHTYPE_386FAST && cpudecoder == &CPU_Core_Prefetch_Run) 
+            {
+                cpudecoder = &CPU_Core_Normal_Run;
+            }
+            else if(CPU_ArchitectureType == CPU_ARCHTYPE_386FAST) 
+            {
+                CPU_ArchitectureType = CPU_ARCHTYPE_386SLOW;
+            }
+            else if(CPU_ArchitectureType == CPU_ARCHTYPE_386SLOW)
+            {
+                CPU_ArchitectureType = CPU_ARCHTYPE_486NEWSLOW;
+                CPU_PrefetchQueueSize = 32;
+                cpudecoder = &CPU_Core_Prefetch_Run;
+            }
+            else if(CPU_ArchitectureType == CPU_ARCHTYPE_486NEWSLOW && cpudecoder == &CPU_Core_Prefetch_Run) 
+            {
+                cpudecoder = &CPU_Core_Normal_Run;
+            }
+            else if(CPU_ArchitectureType == CPU_ARCHTYPE_486NEWSLOW) 
+            {
+                CPU_ArchitectureType = CPU_ARCHTYPE_PENTIUMSLOW;
+            }
+            else if(CPU_ArchitectureType == CPU_ARCHTYPE_PENTIUMSLOW) 
+            {
+                CPU_ArchitectureType = CPU_ARCHTYPE_MIXED;
+            }
+            
+            if(CPU_ArchitectureType >= CPU_ARCHTYPE_486NEWSLOW) CPU_extflags_toggle = (FLAG_ID | FLAG_AC);
+            else if(CPU_ArchitectureType >= CPU_ARCHTYPE_486OLDSLOW) CPU_extflags_toggle = (FLAG_AC);
+            else CPU_extflags_toggle = 0;
+            
+            break;
+            
+        case 5: // Exit
             throw(0);
             break;
     }
@@ -345,7 +406,7 @@ void MENU_Draw(SDL_Surface *surface)
     
     SDL_FillRect(menu.surface, NULL, SDL_MapRGBA(surface->format, 0x00, 0x00, 0xFF, 0xFF));
     
-    for(int i=0; i<5; i++)
+    for(int i=0; i<MENU_ITEMS; i++)
     {
         color = 0xFF;
         
@@ -366,6 +427,7 @@ void MENU_Draw(SDL_Surface *surface)
         if(i == 1) stringRGBA(menu.surface, 125, y, menu.frameskip, color, color, color, 0xFF);
         if(i == 2) stringRGBA(menu.surface, 125, y, menu.cycles, color, color, color, 0xFF);
         if(i == 3) stringRGBA(menu.surface, 125, y, menu.core, color, color, color, 0xFF);
+        if(i == 4) stringRGBA(menu.surface, 125, y, menu.cpuType, color, color, color, 0xFF);
         
         y += 30;
     }
